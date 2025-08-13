24c3aa37fcabccc2409b0a7e344da07d7b9e8f73
teatov <hlebpoimenigleb@gmail.com>
2025-08-13 22:22:47 +1000

    add prettier and eslint and format everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
